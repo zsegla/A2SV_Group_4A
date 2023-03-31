73ad6507bb0abce8b54280cc3f60141fba8e2cdf
Fikreselassie <101448903+zsegla@users.noreply.github.com>
2023-03-31 20:44:15 +0300

    Create decode-string.py

---
+++ decode-string.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/decode-string/description/
+
+
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        repeats = 0
+        digits = set("0123456789")
+        
+        for c in s:
+            if c == "]":
+                item = stack.pop()
+                current = []
+                while not isinstance(item,  int):
+                    current.append(item)
+                    item = stack.pop()
+                stack += (current[::-1] * item)
+            
+            elif c in digits:
+                repeats = repeats * 10 + int(c)
+            
+            elif c == "[":
+                stack.append(repeats)
+                repeats = 0
+                
+            else: 
+                stack.append(c)
+                
+        return "".join(stack)
