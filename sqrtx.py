a5cf8147bad202aef3f6f5698c03a66ad77ddd4d
Fikreselassie <101448903+zsegla@users.noreply.github.com>
2023-03-31 21:03:15 +0300

    Create sqrtx.py

---
+++ sqrtx.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/sqrtx/
+
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        guess = x
+        while guess*guess > x:
+            guess = (guess + x//guess)//2
+        return guess
