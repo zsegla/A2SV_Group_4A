34dba05b939e325101af000157703076c10eb27d
Fikreselassie <101448903+zsegla@users.noreply.github.com>
2023-04-07 09:39:51 +0300

    Create h-index-ii.py

---
+++ h-index-ii.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/h-index-ii/
+
+
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        l, r = 0, n-1
+
+        while l <= r:
+            mid = (l + r)//2
+
+            if citations[mid] >= n - mid:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return n - l
